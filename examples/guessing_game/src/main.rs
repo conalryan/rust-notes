6ad5367e413eefe945e0afca53534e4fd00fb4c1
Conal Ryan <corya@akamai.com>
2021-02-12 19:25:35 -0500

    examples

--- examples/guessing_game/src/main.rs
+++ examples/guessing_game/src/main.rs
@@ -6,7 +6,7 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1..101);
     // println!("The secret number is: {}", secret_number);
 
     loop {
