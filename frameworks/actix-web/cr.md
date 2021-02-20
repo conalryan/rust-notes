68869802d9b1c4015d1efe9332a9e780691c2473
Conal Ryan <corya@akamai.com>
2021-02-20 15:45:38 -0500

    actix-web

---
+++ frameworks/actix-web/cr.md
@@ -0,0 +1,2 @@
+# https://github.com/actix/actix-web
+
---
+++ frameworks/actix-web/hello/src/main.rs
@@ -0,0 +1,15 @@
+use actix_web::{get, web, App, HttpServer, Responder};
+
+// http://localhost:8080/22/bob/index.html
+#[get("/{id}/{name}/index.html")]
+async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
+    format!("Hello {}! id:{}", name, id)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
