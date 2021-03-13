1abe1457f6110a45a99920b92d15aada1ca694c4
Conal Ryan <corya@akamai.com>
2021-03-13 05:37:00 -0500

    actix-web::hello

---
+++ frameworks/actix-web/hello/README.md
@@ -0,0 +1,6 @@
+Quick start
+------------------------------------------------------------------------------
+
+`curl http://localhost:8080/22/bob/index.html`
+
+`curl http://localhost:8080/name/bob`
--- frameworks/actix-web/hello/src/main.rs
+++ frameworks/actix-web/hello/src/main.rs
@@ -1,14 +1,24 @@
 use actix_web::{get, web, App, HttpServer, Responder};
 
-// http://localhost:8080/22/bob/index.html
+// curl http://localhost:8080/22/bob/index.html
 #[get("/{id}/{name}/index.html")]
 async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
     format!("Hello {}! id:{}", name, id)
 }
 
+// curl http://localhost:8080/name/bob
+#[get("/name/{name}")]
+async fn get_name(web::Path(name): web::Path<String>) -> impl Responder {
+    format!("name: {}", name)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(||
+        App::new()
+            .service(index)
+            .service(get_name)
+        )
         .bind("127.0.0.1:8080")?
         .run()
         .await
