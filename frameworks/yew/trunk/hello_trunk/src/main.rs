38c4bf1d2959fafcff5e1c1f814a49ee7c06ae78
Conal Ryan <corya@akamai.com>
2022-08-21 15:41:37 -0400

    feat (frameworks) Yew + Trunk: 3. Click Handler

--- frameworks/yew/trunk/hello_trunk/src/main.rs
+++ frameworks/yew/trunk/hello_trunk/src/main.rs
@@ -1,11 +1,15 @@
 use yew::prelude::*;
 
+enum Msg {
+    AddOne,
+}
+
 struct Model {
     value: i64,
 }
 
 impl Component for Model {
-    type Message = ();
+    type Message = Msg;
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
@@ -16,22 +20,23 @@ impl Component for Model {
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            _ => {
-                // return true to re-render the view
+            Msg::AddOne => {
+                self.value += 1;
+                // the value has changed so we need to
+                // re-render for it to appear on the page
                 true
             }
         }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
+       // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
         let link = ctx.link();
         html! {
-            <>
-                <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-                <h1>{ "Hello World!" }</h1>
-                <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
-            </>
+            <div>
+                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
+                <p>{ self.value }</p>
+            </div>
         }
     }
 }
